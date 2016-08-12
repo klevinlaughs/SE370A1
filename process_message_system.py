e3b8a74944c58591a0bd804179c6021ec4694799
Kelvin Lau <kclau95@gmail.com>
2016-08-13 03:21:18 +1200

    close the pipe

--- process_message_system.py
+++ process_message_system.py
@@ -1,40 +1,41 @@
 import sys, os, queue, pickle
 
 class MessageProc():
-    
+
     def main(self, *args):
         os.mkfifo("/tmp/pipe" + str(os.getpid()))
         print(os.getpid())
         self.queue = queue.Queue()
-        
+
     def start(self, *args):
         pid = os.fork()
         if (pid == 0):
             self.main()
         else:
             return pid
-        
-    def give(self, pid, message, value):
+
+    def give(self, pid, message, *values):
         pipe = open("/tmp/pipe" + str(pid))
-        pickle.dump({message, value}, pipe)
-        
+        pickle.dump({message, values}, pipe)
+        pipe.close()
+
     def receive(self, *args):
-        
+
         something = self.queue.get()
         #pickle.load()
-        
-        
+
+
 class Message():
-    
+
     def __init__(self, data, action, guard = None):
-        
+
         self.data = data
         self.action = action
         self.guard = guard
-        
+
 class TimeOut():
-    
+
     def __init__( self, time, action):
-        
+
         self.time = time
         self.action = action
