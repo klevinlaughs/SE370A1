a12d8b225675c9553c1f8b1c9af278bfd92c1417
Kelvin Lau <kclau95@gmail.com>
2016-08-13 17:28:42 +1200

    Added isExisting to check if pipe exists
    - also passed in *args to self.main in start method
    - some formatting

--- process_message_system.py
+++ process_message_system.py
@@ -2,21 +2,28 @@ import sys, os, queue, pickle
 
 ANY = 'any'
 
+
 class MessageProc():
 
     def main(self, *args):
         os.mkfifo("/tmp/pipe" + str(os.getpid()))
-        print(os.getpid())
+        # print("main:", os.getpid())
         self.queue = queue.Queue()
 
     def start(self, *args):
         pid = os.fork()
         if (pid == 0):
-            self.main()
+            self.main(*args)
         else:
             return pid
 
     def give(self, pid, message, *values):
+        isExisting = os.path.exists("/tmp/pipe" + str(pid))
+        # print("/tmp/pipe" + str(pid))
+        while not isExisting:
+            isExisting = os.path.exists("/tmp/pipe" + str(pid))
+            # print(isExisting)
+
         pipe = open("/tmp/pipe" + str(pid))
         pickle.dump({message, values}, pipe)
         pipe.close()
@@ -24,20 +31,21 @@ class MessageProc():
     def receive(self, *args):
 
         something = self.queue.get()
-        #pickle.load()
+        # pickle.load()
 
 
 class Message():
 
-    def __init__(self, data, action, guard = None):
+    def __init__(self, data, action, guard=None):
 
         self.data = data
         self.action = action
         self.guard = guard
 
+
 class TimeOut():
 
-    def __init__( self, time, action):
+    def __init__(self, time, action):
 
         self.time = time
         self.action = action
